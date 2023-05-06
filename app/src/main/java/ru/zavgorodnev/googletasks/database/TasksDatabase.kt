21199db5c20f3a24a75733f099147c246895a225
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-07 00:12:25 +1000

    Add TaskDatabase class

---
+++ app/src/main/java/ru/zavgorodnev/googletasks/database/TasksDatabase.kt
@@ -0,0 +1,10 @@
+package ru.zavgorodnev.googletasks.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.zavgorodnev.googletasks.data.task.Task
+
+@Database(entities = [ Task::class ], version = 1, exportSchema = false)
+abstract class TaskDatabase : RoomDatabase() {
+
+}
