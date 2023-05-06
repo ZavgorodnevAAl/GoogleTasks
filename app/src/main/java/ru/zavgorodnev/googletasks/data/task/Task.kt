948abae6cfa6511ce6f80c61deab68ba8de9dd97
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-07 00:11:11 +1000

    Re-write task model to room entity

--- app/src/main/java/ru/zavgorodnev/googletasks/data/task/Task.kt
+++ app/src/main/java/ru/zavgorodnev/googletasks/data/task/Task.kt
@@ -1,8 +1,15 @@
 package ru.zavgorodnev.googletasks.data.task
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
 data class Task(
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
     var title: String,
     var description: String,
     var isCompleted: Boolean,
-    var isFavorite: Boolean
+    var isFavorite: Boolean,
+    var parent: UUID?
 )
