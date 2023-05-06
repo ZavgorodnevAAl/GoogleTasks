e051046bca7c4881de541583f50c3953048bba65
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-07 00:46:42 +1000

    Add view model for ListFragment

---
+++ app/src/main/java/ru/zavgorodnev/googletasks/ui/list/ListViewModel.kt
@@ -0,0 +1,28 @@
+package ru.zavgorodnev.googletasks.ui.list
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.zavgorodnev.googletasks.data.task.InDatabaseTaskRepository
+import ru.zavgorodnev.googletasks.data.task.Task
+
+class ListViewModel : ViewModel() {
+
+    private val tasksRepository = InDatabaseTaskRepository.get()
+
+    val tasks: LiveData<List<Task>> = tasksRepository.getTasks()
+    val favoriteTasks: LiveData<List<Task>> = tasksRepository.getFavouriteTasks()
+    val completedTasks: LiveData<List<Task>> = tasksRepository.getCompletedTasks()
+
+    fun addTask(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        tasksRepository.add(task)
+    }
+
+    fun updateTask(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        tasksRepository.updateTask(task)
+    }
+
+
+}
