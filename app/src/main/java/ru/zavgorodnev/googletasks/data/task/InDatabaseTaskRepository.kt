44e2bb8d84c3c022568e727904968f255243a9b6
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-07 00:29:20 +1000

    Add task repository for database implementation

---
+++ app/src/main/java/ru/zavgorodnev/googletasks/data/task/InDatabaseTaskRepository.kt
@@ -0,0 +1,61 @@
+package ru.zavgorodnev.googletasks.data.task
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import ru.zavgorodnev.googletasks.database.TaskDatabase
+import java.util.UUID
+
+
+private const val DATABASE_NAME = "tasks-database"
+
+class InDatabaseTaskRepository private constructor(context: Context) {
+
+    private val database: TaskDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        TaskDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val tasksDao = database.taskDao()
+
+
+    fun getTask(id: UUID): LiveData<Task?> = tasksDao.getTask(id)
+    fun getTasks(): LiveData<List<Task>> = tasksDao.getTasks()
+    fun getCompletedTasks(): LiveData<List<Task>> = tasksDao.getCompletedTasks()
+    fun getFavouriteTasks(): LiveData<List<Task>> = tasksDao.getFavoriteTasks()
+    fun getSubtasks(parentId: UUID): LiveData<List<Task>> = tasksDao.getSubtasks(parentId)
+
+    suspend fun updateTask(task: Task) {
+
+        tasksDao.updateTask(task)
+    }
+
+    suspend fun removeTask(task: Task) {
+
+        tasksDao.deleteTask(task)
+    }
+
+    suspend fun add(task: Task) {
+
+        tasksDao.addTask(task)
+    }
+
+
+    companion object {
+
+        private var INSTANCE: InDatabaseTaskRepository? = null
+
+        fun get(): InDatabaseTaskRepository {
+            return INSTANCE ?: throw IllegalStateException("InDatabaseTaskRepository must be initialize")
+        }
+
+        fun init(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = InDatabaseTaskRepository(context)
+            }
+        }
+
+    }
+
+}
