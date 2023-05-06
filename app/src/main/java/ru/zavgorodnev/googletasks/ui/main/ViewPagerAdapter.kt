68d99290637e9226f29cfdadf953ba902cd81fc2
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-07 06:29:43 +1000

    Add types list

--- app/src/main/java/ru/zavgorodnev/googletasks/ui/main/ViewPagerAdapter.kt
+++ app/src/main/java/ru/zavgorodnev/googletasks/ui/main/ViewPagerAdapter.kt
@@ -4,12 +4,17 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import ru.zavgorodnev.googletasks.ui.list.ListFragment
 
+val types = listOf<String>(
+    ListFragment.COMPLETED_TYPE,
+    ListFragment.ALL_TYPE,
+    ListFragment.FAVORITES_TYPE
+)
 private const val NUMBER_OF_PAGES = 3
 
 class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = NUMBER_OF_PAGES
 
-    override fun createFragment(position: Int): Fragment = ListFragment()
+    override fun createFragment(position: Int): Fragment = ListFragment.newInstance(types[position])
 
 }
