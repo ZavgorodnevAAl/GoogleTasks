1959d091bb4f1de0c349e924b315ddc8db8dfebf
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-07 00:32:54 +1000

    Initialize InDatabaseTaskRepository when app onCreate

--- app/src/main/java/ru/zavgorodnev/googletasks/GoogleTasksApplication.kt
+++ app/src/main/java/ru/zavgorodnev/googletasks/GoogleTasksApplication.kt
@@ -1,6 +1,13 @@
 package ru.zavgorodnev.googletasks
 
 import android.app.Application
+import ru.zavgorodnev.googletasks.data.task.InDatabaseTaskRepository
 
 class GoogleTasksApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        InDatabaseTaskRepository.init(this)
+    }
 }
