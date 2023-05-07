7ad7d9853f3a5c9404595ee6f1bfe15654caafc9
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-08 01:29:19 +1000

    Make MainActivity as Navigator

--- app/src/main/java/ru/zavgorodnev/googletasks/MainActivity.kt
+++ app/src/main/java/ru/zavgorodnev/googletasks/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.zavgorodnev.googletasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import ru.zavgorodnev.googletasks.databinding.ActivityMainBinding
 import ru.zavgorodnev.googletasks.ui.main.MainFragment
+import ru.zavgorodnev.googletasks.utils.Navigator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
 
     lateinit var binding: ActivityMainBinding
 
@@ -19,5 +21,16 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.fragment_container, MainFragment())
                 .commit()
         }
+    }
+
+    override fun launch(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun goBack() {
+        onBackPressed()
     }
 }
