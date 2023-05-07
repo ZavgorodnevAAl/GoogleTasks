48c1397fa8a4d355bb1ffc716bffc68a2e276d8c
ZavgorodnevAAl <zavgorodnev.artem@gmail.com>
2023-05-08 01:59:29 +1000

    Add bottom sheet dialog for creating subtask in DetailFragment

--- app/src/main/java/ru/zavgorodnev/googletasks/ui/detail/DetailFragment.kt
+++ app/src/main/java/ru/zavgorodnev/googletasks/ui/detail/DetailFragment.kt
@@ -7,12 +7,15 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import ru.zavgorodnev.googletasks.R
 import ru.zavgorodnev.googletasks.data.task.Task
+import ru.zavgorodnev.googletasks.databinding.BottomSheetFragmentCreateTaskBinding
 import ru.zavgorodnev.googletasks.databinding.FragmentDetailBinding
 import ru.zavgorodnev.googletasks.utils.Navigator
 import java.util.UUID
@@ -23,6 +26,7 @@ class DetailFragment : Fragment() {
     private val viewModel: DetailViewModel by activityViewModels()
     private lateinit var task: Task
     private var navigator: Navigator? = null
+    private lateinit var taskCreationDialog: BottomSheetDialog
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -73,6 +77,10 @@ class DetailFragment : Fragment() {
             }
         }
 
+        taskCreationDialog = BottomSheetDialog(requireContext(), R.style.DialogStyle)
+        taskCreationDialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        makeTaskCreationDialog()
+
         return binding.root
     }
 
@@ -119,6 +127,54 @@ class DetailFragment : Fragment() {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         override fun afterTextChanged(s: Editable?) { task.description = s.toString() }
+    }
+
+    private fun makeTaskCreationDialog() {
+        val dialogBinding = BottomSheetFragmentCreateTaskBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+
+        dialogBinding.saveTaskButton.isEnabled = false
+
+        dialogBinding.descriptionImageButton.setOnClickListener {
+            dialogBinding.descriptionEditText.visibility = View.VISIBLE
+        }
+        dialogBinding.addToFavoriteCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            dialogBinding.addToFavoriteCheckBox.setButtonDrawable(
+                if (isChecked) R.drawable.ic_star else R.drawable.ic_star_border
+            )
+        }
+
+        dialogBinding.titleEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                dialogBinding.saveTaskButton.isEnabled = s?.isNotBlank() == true
+            }
+        })
+
+        dialogBinding.saveTaskButton.setOnClickListener {
+            val subtask = Task(
+                title = dialogBinding.titleEditText.text.toString(),
+                description = dialogBinding.descriptionEditText.text.toString(),
+                isFavorite = dialogBinding.addToFavoriteCheckBox.isChecked,
+                isCompleted = false,
+                parent = null
+            )
+            viewModel.addSubtask(subtask)
+            taskCreationDialog.dismiss()
+        }
+        dialogBinding.titleEditText.requestFocus()
+
+        taskCreationDialog.setOnDismissListener {
+            dialogBinding.apply {
+                titleEditText.text.clear()
+                descriptionEditText.text.clear()
+                descriptionEditText.visibility = View.GONE
+                addToFavoriteCheckBox.isChecked = false
+            }
+        }
+
+        taskCreationDialog.setContentView(dialogBinding.root)
     }
 
     companion object {
